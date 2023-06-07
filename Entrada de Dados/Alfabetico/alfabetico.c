1084ef59dab4315a3205422131b2192eaf32b691
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-07 01:33:21 -0300

    Update alfabetico.c

--- Entrada de Dados/Alfabetico/alfabetico.c
+++ Entrada de Dados/Alfabetico/alfabetico.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 char TEXTO[100];
-size_t I;
-bool VALIDACAO;
-  
+
 int main(void) {
+  
+  bool VALIDACAO = false;
+
   do {
     printf("Entre uma cadeia alfabetica: ");
     fgets(TEXTO, sizeof(TEXTO), stdin);
     TEXTO[strcspn(TEXTO, "\n")] = '\0';
     VALIDACAO = true;
-    for (I = 0; I < strlen(TEXTO); I++) {
-      char CARACTERE = TEXTO[I];
-      if (!((CARACTERE >= 'A' && CARACTERE <= 'Z') || 
-            (CARACTERE >= 'a' && CARACTERE <= 'z') || 
-             CARACTERE == ' ')) {
+    for (size_t I = 0; TEXTO[I] != '\0'; I++) {
+      if (!isalpha(TEXTO[I]) && TEXTO[I] != ' ') {
         VALIDACAO = false;
         break;
       }
     }
-    if (VALIDACAO == false) {
+    if (VALIDACAO  == false) {
       printf("Entrada invalida. Por favor, tente novamente.\n");
     } else {
       break;
