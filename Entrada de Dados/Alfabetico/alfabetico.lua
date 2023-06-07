fc65f94711c9d994ea086bed57e1c43c6abb02e0
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-07 00:56:31 -0300

    Update alfabetico.lua

--- Entrada de Dados/Alfabetico/alfabetico.lua
+++ Entrada de Dados/Alfabetico/alfabetico.lua
@@ -1,18 +1,19 @@
 TEXTO = ""
 I = 1
 VALIDACAO = false
+CARACTERE = ''
 
 repeat
   io.write("Entre uma cadeia alfabetica: ")
   TEXTO = io.read()
   TEXTO = string.gsub(TEXTO, "\n", "")
-  VALIDACAO = true
+  VALIDACAO = false
   for I = 1, string.len(TEXTO) do
-    CARACTERE = string.sub(TEXTO, I, I)
-    if not (('A' <= CARACTERE and CARACTERE <= 'Z') or 
-            ('a' <= CARACTERE and CARACTERE <= 'z') or 
-                    (CARACTERE == ' ')) then
-      VALIDACAO = false
+     CARACTERE = string.sub(TEXTO, I, I)
+    if  (('A' <= CARACTERE and CARACTERE <= 'Z') or 
+         ('a' <= CARACTERE and CARACTERE <= 'z') or 
+                              (CARACTERE == ' ')) then
+      VALIDACAO = true
       break
     end
   end
@@ -20,4 +21,5 @@ repeat
     print("Entrada invalida. Por favor, tente novamente.")
   end
 until VALIDACAO
+
 print("Voce informou a cadeia: " .. TEXTO)
