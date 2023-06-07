e36a5cb1043783095c5d9456e2f4b19ad030ed61
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-07 00:31:40 -0300

    Update alfabetico.scala

--- Entrada de Dados/Alfabetico/alfabetico.scala
+++ Entrada de Dados/Alfabetico/alfabetico.scala
@@ -2,25 +2,28 @@ import scala.io.StdIn.readLine
 
 object Main {
   def main(args: Array[String]): Unit = {
-    
+
     var TEXTO: String = ""
     var VALIDACAO: Boolean = false
 
     while (true) {
-      print("Entre uma cadeia alfabetica: ")
+      print("Entre uma cadeia alfabética: ")
       TEXTO = readLine()
       VALIDACAO = true
+
       for (I <- 0 until TEXTO.length) {
         val CARACTERE = TEXTO.charAt(I)
-        if (!(CARACTERE.isUpper || CARACTERE.isLower || 
-              CARACTERE == ' ')) {
+        if (!(CARACTERE.isLetter || CARACTERE.isWhitespace)) {
           VALIDACAO = false
         }
       }
+
       if (VALIDACAO == false) {
-        println("Entrada invalida. Por favor, tente novamente.")
+        println("Entrada inválida. Por favor, tente novamente.")
+      } else {
+        println(s"Você informou a cadeia: $TEXTO")
+        return
       }
     }
-    println(s"Voce informou a cadeia: $TEXTO")
   }
 }
