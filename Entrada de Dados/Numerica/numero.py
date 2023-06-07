9d19620cd7fa2b58c9a769da18a6fb11b571810c
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-07 10:34:51 -0300

    Delete Entrada de Dados/Numerica directory

--- Entrada de Dados/Numerica/numero.py
+++
@@ -1,8 +0,0 @@
-NUMERO = None
-while (True):
-  try:
-    NUMERO = int(input("Entre um numero: "))
-    break
-  except (ValueError):
-    print("Entrada invalida. Por favor, tente novamente.")
-print("Voce informou o numero:", NUMERO)
