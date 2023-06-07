762c1345be9f60b20b502546fa1f4a88111351c3
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-07 01:12:10 -0300

    Update alfabetico.dart

--- Entrada de Dados/Alfabetico/alfabetico.dart
+++ Entrada de Dados/Alfabetico/alfabetico.dart
@@ -1,29 +1,18 @@
 import 'dart:io';
 
 void main() {
-  
   String TEXTO;
-  int I;
   bool VALIDACAO;
 
   do {
     stdout.write('Entre uma cadeia alfabética: ');
     TEXTO = stdin.readLineSync()!;
-    VALIDACAO = true;
-    for (I = 0; I < TEXTO.length; I++) {
-      String CARACTERE = TEXTO[I];
-      if (!((CARACTERE.compareTo('A') >= 0 && CARACTERE.compareTo('Z') <= 0) ||
-            (CARACTERE.compareTo('a') >= 0 && CARACTERE.compareTo('z') <= 0) ||
-             CARACTERE == ' ')) {
-        VALIDACAO = false;
-        break;
-      }
-    }
-    if (VALIDACAO == false) {
+    VALIDACAO = TEXTO.split('').any((CARACTERE) => RegExp(r'[a-zA-Z ]').hasMatch(CARACTERE));
+    if (!VALIDACAO) {
       print('Entrada inválida. Por favor, tente novamente.');
     } else {
       break;
     }
   } while (true);
-  print('Você informou a cadeia: $TEXTO');
+    print('Você informou a cadeia: $TEXTO');
 }
