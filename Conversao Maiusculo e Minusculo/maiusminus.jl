47ccdcaec93eac13bfb1b466325b060bacabc0ac
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-08 01:37:15 -0300

    Delete maiusminus.jl

--- Conversao Maiusculo e Minusculo/maiusminus.jl
+++
@@ -1,8 +0,0 @@
-print("Digite um texto .....: ")
-texto = readline()
-
-textoMaiusculo = uppercase(texto)
-textoMinusculo = lowercase(texto)
-
-println("Texto em maiusculo ..: ", textoMaiusculo)
-println("Texto em minusculo ..: ", textoMinusculo)
