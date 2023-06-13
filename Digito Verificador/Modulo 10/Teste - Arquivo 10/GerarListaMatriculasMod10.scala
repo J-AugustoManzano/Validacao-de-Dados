70730c6530bbb601fcc7eca1d59745bac61cbf9a
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-13 14:07:19 -0300

    Update GerarListaMatriculasMod10.scala

--- Digito Verificador/Modulo 10/Teste - Arquivo 10/GerarListaMatriculasMod10.scala
+++ Digito Verificador/Modulo 10/Teste - Arquivo 10/GerarListaMatriculasMod10.scala
@@ -1,48 +1,48 @@
 import java.io.PrintWriter
 
 object Main {
-  def formataMatricula(matricula: Int): String = {
-    f"$matricula%06d"
+  def formataMatricula(MATRICULA: Int): String = {
+    f"$MATRICULA%06d"
   }
 
-  def mod10(matricula: String): Int = {
-    var soma = 0
+  def mod10(MATRICULA: String): Int = {
+    var SOMA = 0
     for (i <- 0 until 6) {
-      val digito = matricula(i) - '0'
-      if (i % 2 != 0 && digito * 2 > 9)
-        soma += digito * 2 - 9
-      else if (i % 2 != 0 && digito * 2 <= 9)
-        soma += digito * 2
+      val DIGITO = MATRICULA(i) - '0'
+      if (i % 2 != 0 && DIGITO * 2 > 9)
+        SOMA += DIGITO * 2 - 9
+      else if (i % 2 != 0 && DIGITO * 2 <= 9)
+        SOMA += DIGITO * 2
       else
-        soma += digito
+        SOMA += DIGITO
     }
-    val dv = if (soma % 10 == 0) 0 else 10 - soma % 10
-    dv
+    val DV = if (SOMA % 10 == 0) 0 else 10 - SOMA % 10
+    DV
   }
 
   def main(args: Array[String]): Unit = {
-    val arquivo = new PrintWriter("numeros.txt")
+    val ARQUIVO = new PrintWriter("numeros.txt")
 
     println("GERADOR DE NUMEROS DE MATRICULA DE 000.000 ATE 999.999 COM \"DV\"")
     println("Padrao: Modulo 10")
     println("---------------------------------------------------------------")
     println()
 
-    if (arquivo == null) {
-      Console.err.println("Erro ao abrir o arquivo.")
+    if (ARQUIVO == null) {
+      Console.err.println("Erro ao abrir o ARQUIVO.")
       println("Programa encerrado.")
       return
     }
 
-    print("Aguarde arquivo sendo gerado... ")
+    print("Aguarde ARQUIVO sendo gerado... ")
 
-    for (i <- 0 to 999999) {
-      val matricula = formataMatricula(i)
-      val dv = mod10(matricula)
-      arquivo.println(s"${matricula.substring(0, 3)}.${matricula.substring(3)}-$dv")
+    for (I <- 0 to 999999) {
+      val MATRICULA = formataMatricula(I)
+      val DV = mod10(MATRICULA)
+      ARQUIVO.println(s"${MATRICULA.substring(0, 3)}.${MATRICULA.substring(3)}-$DV")
     }
 
-    arquivo.close()
+    ARQUIVO.close()
 
     println("\n\nSUCESSO: Arquivo gerado.\n")
   }
