4229a44f2668256ae432512081c0cabdb2adfc5b
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-13 14:15:36 -0300

    Update GerarListaMatriculasMod10.java

--- Digito Verificador/Modulo 10/Teste - Arquivo 10/GerarListaMatriculasMod10.java
+++ Digito Verificador/Modulo 10/Teste - Arquivo 10/GerarListaMatriculasMod10.java
@@ -4,46 +4,46 @@ import java.io.PrintWriter;
 
 public class Main {
 
-    public static String formataMatricula(int matricula) {
-        return String.format("%06d", matricula);
+    public static String formataMatricula(int MATRICULA) {
+        return String.format("%06d", MATRICULA);
     }
 
-    public static int mod10(String matricula) {
-        int soma = 0;
-        for (int i = 0; i < 6; i++) {
-            int digito = Character.getNumericValue(matricula.charAt(i));
-            if (i % 2 != 0 && digito * 2 > 9) {
-                soma += digito * 2 - 9;
-            } else if (i % 2 != 0 && digito * 2 <= 9) {
-                soma += digito * 2;
+    public static int mod10(String MATRICULA) {
+        int SOMA = 0;
+        for (int I = 0; I <= 5; I++) {
+            int DIGITO = Character.getNumericValue(MATRICULA.charAt(I));
+            if (I % 2 != 0 && DIGITO * 2 > 9) {
+                SOMA += DIGITO * 2 - 9;
+            } else if (I % 2 != 0 && DIGITO * 2 <= 9) {
+                SOMA += DIGITO * 2;
             } else {
-                soma += digito;
+                SOMA += DIGITO;
             }
         }
-        int dv = (soma % 10 == 0) ? 0 : 10 - soma % 10;
-        return dv;
+        int DV = (SOMA % 10 == 0) ? 0 : 10 - SOMA % 10;
+        return DV;
     }
 
     public static void main(String[] args) {
-        String arquivo = "numeros.txt";
+        String ARQUIVO = "numeros.txt";
 
-        try (PrintWriter writer = new PrintWriter(new FileWriter(arquivo))) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(ARQUIVO))) {
             System.out.println("GERADOR DE NUMEROS DE MATRICULA DE 000.000 ATE 999.999 COM \"DV\"");
             System.out.println("Padrao: Modulo 10");
             System.out.println("---------------------------------------------------------------");
             System.out.println();
 
-            System.out.print("Aguarde arquivo sendo gerado... ");
+            System.out.print("Aguarde ARQUIVO sendo gerado... ");
 
-            for (int i = 0; i <= 999999; i++) {
-                String matricula = formataMatricula(i);
-                int dv = mod10(matricula);
-                writer.println(matricula.substring(0, 3) + "." + matricula.substring(3) + "-" + dv);
+            for (int I = 0; I <= 999999; I++) {
+                String MATRICULA = formataMatricula(I);
+                int DV = mod10(MATRICULA);
+                writer.println(MATRICULA.substring(0, 3) + "." + MATRICULA.substring(3) + "-" + DV);
             }
 
             System.out.println("\n\nSUCESSO: Arquivo gerado.\n");
         } catch (IOException e) {
-            System.err.println("Erro ao abrir o arquivo.");
+            System.err.println("Erro ao abrir o ARQUIVO.");
             System.err.println("Programa encerrado.");
         }
     }
