61773796fe65b4c023922d4ab9d07cab6e349f6f
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-13 14:30:22 -0300

    Update GerarListaMatriculasMod10.go

--- Digito Verificador/Modulo 10/Teste - Arquivo 10/GerarListaMatriculasMod10.go
+++ Digito Verificador/Modulo 10/Teste - Arquivo 10/GerarListaMatriculasMod10.go
@@ -5,49 +5,49 @@ import (
 	"os"
 )
 
-func formataMatricula(matricula int) string {
-	return fmt.Sprintf("%06d", matricula)
+func formataMatricula(MATRICULA int) string {
+	return fmt.Sprintf("%06d", MATRICULA)
 }
 
-func mod10(matricula string) int {
-	soma := 0
-	for i := 0; i < 6; i++ {
-		digito := int(matricula[i] - '0')
-		if i%2 != 0 && digito*2 > 9 {
-			soma += digito*2 - 9
-		} else if i%2 != 0 && digito*2 <= 9 {
-			soma += digito * 2
+func mod10(MATRICULA string) int {
+	SOMA := 0
+	for I := 0; I <= 5; I++ {
+		DIGITO := int(MATRICULA[I] - '0')
+		if I % 2 != 0 && DIGITO * 2 > 9 {
+			SOMA += DIGITO * 2 - 9
+		} else if I % 2 != 0 && DIGITO * 2 <= 9 {
+			SOMA += DIGITO * 2
 		} else {
-			soma += digito
+			SOMA += DIGITO
 		}
 	}
-	dv := (soma % 10)
-	if dv != 0 {
-		dv = 10 - dv
+	DV := (SOMA % 10)
+	if DV != 0 {
+		DV = 10 - DV
 	}
-	return dv
+	return DV
 }
 
 func main() {
-	arquivo, err := os.Create("numeros.txt")
-	if err != nil {
-		fmt.Println("Erro ao abrir o arquivo.")
+	ARQUIVO, ERRO := os.Create("numeros.txt")
+	if ERRO != nil {
+		fmt.Println("Erro ao abrir o ARQUIVO.")
 		fmt.Println("Programa encerrado.")
 		return
 	}
-	defer arquivo.Close()
+	defer ARQUIVO.Close()
 
 	fmt.Println("GERADOR DE NUMEROS DE MATRICULA DE 000.000 ATE 999.999 COM \"DV\"")
 	fmt.Println("Padrao: Modulo 10")
 	fmt.Println("---------------------------------------------------------------")
 	fmt.Println()
 
-	fmt.Print("Aguarde arquivo sendo gerado... ")
+	fmt.Print("Aguarde ARQUIVO sendo gerado... ")
 
-	for i := 0; i <= 999999; i++ {
-		matricula := formataMatricula(i)
-		dv := mod10(matricula)
-		fmt.Fprintf(arquivo, "%s.%s-%d\n", matricula[:3], matricula[3:], dv)
+	for I := 0; I <= 999999; I++ {
+		MATRICULA := formataMatricula(I)
+		DV := mod10(MATRICULA)
+		fmt.Fprintf(ARQUIVO, "%s.%s-%d\n", MATRICULA[:3], MATRICULA[3:], DV)
 	}
 
 	fmt.Println("\n\nSUCESSO: Arquivo gerado.\n")
