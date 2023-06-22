4fc7ad8451ece7c94aac715a7d939bbcf93b472d
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-22 20:22:38 -0300

    Update milhares.py

--- Formatacao de Milhares/milhares.py
+++ Formatacao de Milhares/milhares.py
@@ -1,4 +1,4 @@
-def formata_valor(valor):
+def FormataValor(valor):
     # Verifica se o valor possui parte decimal
     if '.' in valor:
         inteiro, decimal = valor.split('.')
@@ -6,14 +6,14 @@ def formata_valor(valor):
         inteiro = valor
         decimal = '00'
     # Formata a parte inteira
-    valor_formatado = ''
+    valorFormatado = ''
     for i in range(len(inteiro), 0, -1):
-        valor_formatado = inteiro[i-1] + valor_formatado
+        valorFormatado = inteiro[i-1] + valorFormatado
         if i > 1 and (len(inteiro) - i + 1) % 3 == 0:
-            valor_formatado = '.' + valor_formatado
-    valor_formatado = valor_formatado + ',' + decimal
-    return valor_formatado
+            valorFormatado = '.' + valorFormatado
+    valorFormatado = valorFormatado + ',' + decimal
+    return valorFormatado
 
 valor = input('Digite um valor real: ')
-valor_formatado = formata_valor(valor)
-print('Valor formatado:', valor_formatado)
+valorFormatado = FormataValor(valor)
+print('Valor formatado:', valorFormatado)
