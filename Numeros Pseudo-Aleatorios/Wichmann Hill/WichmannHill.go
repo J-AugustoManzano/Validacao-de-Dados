1d6b09f8b808c962a0dfa11467d0f4203aa79152
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-23 07:52:38 -0300

    Update WichmannHill.go

--- Numeros Pseudo-Aleatorios/Wichmann Hill/WichmannHill.go
+++ Numeros Pseudo-Aleatorios/Wichmann Hill/WichmannHill.go
@@ -7,41 +7,36 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"time"
+    "fmt"
+    "math"
+    "time"
 )
 
 func WichmannHill(X *int) float64 {
-	*X = (171 * *X) % 30269
-	return math.Abs(float64(*X) / 30269.0)
+    *X = (171 * *X) % 30269
+    return math.Abs(float64(*X) / 30269.0)
 }
 
 func main() {
-	fmt.Println("GERADOR DE NUMEROS PSEUDO ALEATORIOS (GNA)")
-	fmt.Println("PSEUDORANDOM NUMBER GENERATOR (PRNG)")
-	fmt.Println("PADRAO: Wichmann Hill")
-	fmt.Println("==========================================")
-	fmt.Println()
-	fmt.Println("    Semente: 1234        Semente: TIME")
-	fmt.Println("    -------------        -------------")
+    fmt.Println("GERADOR DE NUMEROS PSEUDO ALEATORIOS (GNA)")
+    fmt.Println("PSEUDORANDOM NUMBER GENERATOR (PRNG)")
+    fmt.Println("PADRAO: Wichmann Hill")
+    fmt.Println("==========================================")
+    fmt.Println()
+    fmt.Println("    Semente: 1234        Semente: TIME")
+    fmt.Println("    -------------        -------------")
 
-	var Xn1 [10]float64
-	var Xn2 [10]float64
-	var X1, X2 int
+    var Xn1 [10]float64
+    var Xn2 [10]float64
+    var X1, X2 int
 
-	X1 = 1234
-	for i := 0; i <= 9; i++ {
-		Xn1[i] = WichmannHill(&X1)
-	}
+    X1 = 1234
+    X2 = int(time.Now().Unix())
 
-	X2 = int(time.Now().Unix())
-	for i := 0; i <= 9; i++ {
-		Xn2[i] = WichmannHill(&X2)
-	}
-
-	for i := 0; i <= 9; i++ {
-		fmt.Printf("%17.11f        ", Xn1[i])
-		fmt.Printf("%13.11f\n", Xn2[i])
-	}
+    for i := 0; i <= 9; i++ {
+        Xn1[i] = WichmannHill(&X1)
+        Xn2[i] = WichmannHill(&X2)
+        fmt.Printf("%17.11f        ", Xn1[i])
+        fmt.Printf("%13.11f\n", Xn2[i])
+    }
 }
