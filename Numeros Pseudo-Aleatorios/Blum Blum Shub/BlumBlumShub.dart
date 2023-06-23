9b3e5f45a5453a48e16df5ac6cebfa1d077192e2
Augusto Manzano <81631170+J-AugustoManzano@users.noreply.github.com>
2023-06-23 08:56:28 -0300

    Update BlumBlumShub.dart

--- Numeros Pseudo-Aleatorios/Blum Blum Shub/BlumBlumShub.dart
+++ Numeros Pseudo-Aleatorios/Blum Blum Shub/BlumBlumShub.dart
@@ -20,20 +20,16 @@ void main() {
   print("    -------------        -------------");
 
   BigInt X1 = BigInt.from(1234);
+  BigInt X2 = BigInt.from(DateTime.now().millisecondsSinceEpoch);
+  
   List<BigInt> Xn1 = List<BigInt>.filled(10, BigInt.zero);
+  List<BigInt> Xn2 = List<BigInt>.filled(10, BigInt.zero);
+
   for (int i = 0; i <= 9; i++) {
     Xn1[i] = blumBlumShub(X1);
-    X1 = Xn1[i];
-  }
-
-  BigInt X2 = BigInt.from(DateTime.now().millisecondsSinceEpoch);
-  List<BigInt> Xn2 = List<BigInt>.filled(10, BigInt.zero);
-  for (int i = 0; i <= 9; i++) {
     Xn2[i] = blumBlumShub(X2);
+    X1 = Xn1[i];
     X2 = Xn2[i];
-  }
-
-  for (int i = 0; i <= 9; i++) {
     print('${Xn1[i].toString().padLeft(17)}        ${Xn2[i].toString().padLeft(13)}');
   }
   
